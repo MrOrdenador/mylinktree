1cf07e629882ae097b42a40d01265b2757637da2
MrOrdenador <132808158+MrOrdenador@users.noreply.github.com>
2024-05-18 14:35:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+    const body = document.querySelector('body');
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        document.getElementById('discord-iframe').src = "https://widgets.vendicated.dev/user?id=1102632671655493733&theme=light&banner=false&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=true&background-color=%23FFF&foreground-color=%23000";
+    } else {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+        document.getElementById('discord-iframe').src = "https://widgets.vendicated.dev/user?id=1102632671655493733&theme=dark&banner=false&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=true&background-color=%23111&foreground-color=%23AAA";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const collapsibles = document.querySelectorAll(".collapsible");
+    collapsibles.forEach(item => {
+        item.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
