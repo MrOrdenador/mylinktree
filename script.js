ab21b79c20af4d3836df149edf91b70fb86193ed
MrOrdenador <132808158+MrOrdenador@users.noreply.github.com>
2024-06-09 13:21:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function toggleMode() {
     const body = document.querySelector('body');
+    const iframe = document.getElementById('discord-iframe');
     if (body.classList.contains('dark-mode')) {
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
-        document.getElementById('discord-iframe').src = "%https://widgets.vendicated.dev/user?id=1244715425137295451&theme=dark&banner=false&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=true&background-color=%23111&foreground-color=23000";
+        iframe.src = "https://widgets.vendicated.dev/user?id=1244715425137295451&theme=light&banner=false&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=true&background-color=%23FFF&foreground-color=%23000";
     } else {
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
-        document.getElementById('discord-iframe').src = "https://widgets.vendicated.dev/user?id=1244715425137295451&theme=dark&banner=false&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=true&background-color=%23111&foreground-color=%23AAA";
+        iframe.src = "https://widgets.vendicated.dev/user?id=1244715425137295451&theme=dark&banner=false&full-banner=true&rounded-corners=true&discord-icon=true&badges=true&guess-nitro=true&background-color=%23111&foreground-color=%23AAA";
     }
 }
 
